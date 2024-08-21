60d6334e51d218e9c069e6deb3fd96707a246b88
PardeepSB <pbhattal19@gmail.com>
2024-08-20 17:44:20 -0700

    updated inc files, created function prototypes

---
+++ SourceCode/inc/main.h
@@ -0,0 +1,41 @@
+/* 
+ * Main.h
+ *
+ * Created on Aug 20, 2024
+ * 	Authors: Pardeep, Manraj, Gursewak
+ *
+ * Description:
+ * 	Main files 
+ *
+ * Model:
+ * 	GY-521
+ *
+ */
+
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------*/
+#include <math.h>
+#include <stdlib.h>
+
+
+/* Global Variables ----------------------------------------------------*/
+
+
+
+/* Global Function Prototypes ------------------------------------------*/
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* INC_MPU6050_H_
